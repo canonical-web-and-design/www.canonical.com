19f595b84485cef7ec2324e28e58838622fb58ea
Robin Winslow <robin@robinwinslow.co.uk>
2014-04-04 21:36:36 +0100

    Urls: Don't use admin, use fenchurch.TemplateFinder

--- canonical/urls.py
+++ canonical/urls.py
@@ -1,12 +1,6 @@
 from django.conf.urls import patterns, include, url
-
-from django.contrib import admin
-admin.autodiscover()
+from fenchurch import TemplateFinder
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'canonical.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<template>.*)$', TemplateFinder.as_view()),
 )
