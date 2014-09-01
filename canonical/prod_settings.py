1cb1009705bd6fc28c9f038ecc6770c1b0a54509
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-01 19:24:09 +0100

    Remove unneeded TEMPLATE_CONTEXT_PROCESSORS defaults; add comments

--- canonical/prod_settings.py
+++ canonical/prod_settings.py
@@ -14,8 +14,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Keep it secret, keep it safe!
 SECRET_KEY = 'o@kjnphb9#+3fl80i#$v$+0la3u^atow)b33h*bafbcwir0w04'
 
+# See https://docs.djangoproject.com/en/dev/ref/contrib/
 INSTALLED_APPS = (
-    'django.contrib.staticfiles',  # Needed for STATIC_URL to work
+    'django.contrib.staticfiles',  # Needed for STATICFILES_DIRS to work
 )
 
 ROOT_URLCONF = 'canonical.urls'
@@ -28,3 +29,8 @@ USE_TZ = True
 TEMPLATE_DIRS = (BASE_DIR + "/templates")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(TEMPLATE_DIRS, "static"), )
+
+# See http://tinyurl.com/django-context-processors
+TEMPLATE_CONTEXT_PROCESSORS = (
+    "django.core.context_processors.static",  # Provides STATIC_URL
+)
