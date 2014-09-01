a3b79735c0cab2858ae8ce392b1eb25f1deb6df8
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-01 18:58:32 +0100

    Make settings even more minimal

--- canonical/prod_settings.py
+++ canonical/prod_settings.py
@@ -14,30 +14,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # Keep it secret, keep it safe!
 SECRET_KEY = 'o@kjnphb9#+3fl80i#$v$+0la3u^atow)b33h*bafbcwir0w04'
 
-DEBUG = True
-
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+INSTALLED_APPS = ('django.contrib.staticfiles',)
 
 ROOT_URLCONF = 'canonical.urls'
-
 WSGI_APPLICATION = 'canonical.wsgi.application'
-
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
-
-INSTALLED_APPS = (
-    'canonical',
-    'django.contrib.staticfiles'
-)
 TEMPLATE_DIRS = (BASE_DIR + "/templates")
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(TEMPLATE_DIRS, "static"), )
--- canonical/settings.py
+++ canonical/settings.py
@@ -1,3 +1,5 @@
 from prod_settings import *
 
 INSTALLED_APPS += ('django_extensions',)
+DEBUG = True
+TEMPLATE_DEBUG = True
