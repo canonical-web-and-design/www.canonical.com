7b7462646aa125e4885568044025e07f13e0e4d4
Robin Winslow <robin@robinwinslow.co.uk>
2014-09-14 13:01:02 +0100

    Simplify urls.py

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,17 +1,11 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from fenchurch import TemplateFinder
 from views import custom_404, custom_500
-from django.conf import settings
-from django.conf.urls.static import static
 
 # Standard patterns
-urlpatterns = patterns(
-    '',
+urlpatterns = [
     url(r'^(?P<template>.*)/?$', TemplateFinder.as_view()),  # Fenchurch
-)
-
-# Static files
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
 
 # Error handlers
 handler404 = custom_404
