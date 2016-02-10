03bcaf816f1d54e4826a3f8465e01a3168a145e5
Robin Winslow <robin@robinwinslow.co.uk>
2016-02-10 13:54:11 +0000

    Remove custom error view methods - breaking everything

--- webapp/views.py
+++ webapp/views.py
@@ -17,13 +17,3 @@ class CanonicalTemplateFinder(TemplateFinder):
             context["level_" + str(index + 1)] = path
 
         return context
-
-
-def custom_404(request):
-    t = loader.get_template('templates/404.html')
-    context = RequestContext(request, {'request_path': request.path})
-    return HttpResponseNotFound(t.render(context))
-
-def custom_500(request):
-    t = loader.get_template('templates/500.html')
-    return HttpResponseServerError(t.render(Context({})))
