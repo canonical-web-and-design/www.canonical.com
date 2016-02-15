98ff7bb019534ce71b95edea5266fcc6b7c4f9e1
Karl Williams <karl@deadlight.net>
2016-02-15 10:24:23 +0000

    JS indentation fix

--- static/js/scratch.js
+++ static/js/scratch.js
@@ -235,40 +235,40 @@ YUI().use('node', 'cookie', 'event-resize', 'event', 'jsonp', 'json-parse', func
 });
 
 core.rssLoader = function(feedurl, jobType, htmlelement, joblimit) {
-    $.getFeed({
-        url: feedurl,
-        success: function(feed) {
-            var html = '';
-            /* correct grammar for 0 and 1 on total list item */
-            if (feed.items.length === 0) {
-                html += '<li class="total">Sorry, we currently have no ' + jobType + ' vacancies';
-            } else if (feed.items.length === 1) {
-                html += '<li class="total">We currently have ' + feed.items.length + ' ' + jobType + ' vacancy';
-            } else {
-                html += '<li class="total">We currently have ' + feed.items.length + ' ' + jobType + ' vacancies';
-            }
+  $.getFeed({
+    url: feedurl,
+    success: function(feed) {
+      var html = '';
+      /* correct grammar for 0 and 1 on total list item */
+      if (feed.items.length === 0) {
+        html += '<li class="total">Sorry, we currently have no ' + jobType + ' vacancies';
+      } else if (feed.items.length === 1) {
+        html += '<li class="total">We currently have ' + feed.items.length + ' ' + jobType + ' vacancy';
+      } else {
+        html += '<li class="total">We currently have ' + feed.items.length + ' ' + jobType + ' vacancies';
+      }
 
-            /* set a default limit for number of jobs returned */
-            if (joblimit == undefined) {
-                joblimit = 5;
-            }
+      /* set a default limit for number of jobs returned */
+      if (joblimit == undefined) {
+        joblimit = 5;
+      }
 
-            for(var i = 0; i < feed.items.length && i < joblimit; i++) {
-                var item = feed.items[i];
-                html += "<li><a href='" + item.link + "'>" + item.title + "&nbsp;&rsaquo;</a></li>";
-            }
+      for(var i = 0; i < feed.items.length && i < joblimit; i++) {
+        var item = feed.items[i];
+        html += "<li><a href='" + item.link + "'>" + item.title + "&nbsp;&rsaquo;</a></li>";
+      }
 
-            /* hide the gif spinner */
-            $('.spinner').hide();
-            $(htmlelement).append(html);
-        }
-    })
+      /* hide the gif spinner */
+      $('.spinner').hide();
+      $(htmlelement).append(html);
+    }
+  })
 };
 
 core.getInsightsFeed = function(url, maxItems, htmlID, type) {
-    $.getFeed({
-      url: url,
-      success: function(feed) {
+  $.getFeed({
+    url: url,
+    success: function(feed) {
       var html = "";
       for(var i = 0; i < feed.items.length && i < maxItems; i++) {
         var item = feed.items[i];
@@ -278,6 +278,7 @@ core.getInsightsFeed = function(url, maxItems, htmlID, type) {
           html += "<li><a href='{0}'>{1}</a><p class='note'><time pubdate datetime='{2}'>{3}</time></p></li>".format(item.link, item.title, item.updated, item.nicedate);
         }
       }
+
       if ($(htmlID)) {
         $(htmlID).append(html);
       }
