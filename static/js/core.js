9323a19532b229d1e9afd38616429f92804f8263
Robin Winslow <robin@robinwinslow.co.uk>
2016-11-05 22:42:19 +0000

    Replace sectionTabs with new setupTabs
    - Replace YUI with plain JavaScript
    - Move to core.js
    - Call the function from the bottom of each page that needs it

--- static/js/core.js
+++ static/js/core.js
@@ -8,6 +8,7 @@
 
 // var core = {};
 
+
 /**
  * Show the cookie notification at the bottom of the page,
  * unless it has previously been dismissed
@@ -42,4 +43,37 @@ core.cookiePolicy = function() {
       createCookieNotification();
     }
   }
+
+/**
+ * Given a set of anchor triggers and a set of tab items,
+ * where the href of the anchors is equivalent to the ID of the target item:
+ * Add class 'hide' to all items except the one which is triggered,
+ * and add the class 'active' to the active anchor
+ *
+ * <a href="#one">anchor 1</a> <a href="#two active">anchor 2</a>
+ * <div id="one hide">item 1</div> <div id="two">item 2</div>
+ */
+core.setupTabs = function (menuAnchors, tabItems) {
+  menuAnchors.forEach(function(anchor) {
+    anchor.addEventListener(
+      'click',
+      function(menuAnchors, tabItems) {
+        return function(clickEvent) {
+          clickEvent.preventDefault();
+          anchor = clickEvent.target.closest('a');
+
+          // Get the ID for this item from the anchor's hash value
+          itemId = anchor.hash;
+
+          // Hide all items but display this one
+          tabItems.forEach(function(item) {item.classList.add('hide');});
+          document.querySelector(itemId).classList.remove('hide');
+
+          // Deactivate other items and activate this one
+          menuAnchors.forEach(function(anchor) {anchor.classList.remove('active');});
+          anchor.classList.add('active');
+        };
+      }(menuAnchors, tabItems)
+    );
+  });
 };
