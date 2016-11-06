b4e1e4245cac9fd1f56b5dcc4127a6004f50e9c3
Robin Winslow <robin@robinwinslow.co.uk>
2016-11-06 00:34:59 +0000

    Use core namespace properly
    The only reason core was being instantiated inside the HTML was to setup the global nav elements. With the new version of the nav this is no longer necessary, so remove it, and let core.js set up the core namespace as it was intented to.

--- static/js/core.js
+++ static/js/core.js
@@ -1,12 +1,10 @@
 /* Setup namespace
- * TODO: Uncomment this once all core.* items have been removed from scratch.js
  */
+if (typeof core !== 'undefined') {
+  throw TypeError("Namespace 'core' not available");
+}
 
-// if (typeof core !== 'undefined') {
-//   throw TypeError("Namespace 'core' not available");
-// }
-
-// var core = {};
+var core = {};
 
 
 /**
