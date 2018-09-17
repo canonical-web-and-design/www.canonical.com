1d6864698a41115996b3aee200e5bcc4b27c9615
Peter Mahnke <peter@transitionelement.com>
2018-09-17 15:17:47 +0200

    add view for secure boot master certificate

--- webapp/views.py
+++ webapp/views.py
@@ -4,9 +4,15 @@ Django views for www.canonical.com.
 from django.conf import settings
 from django.views.generic.base import TemplateView
 from django_template_finder_view import TemplateFinder
-
 from webapp.templatetags.raw_feeds import get_raw_json_feed
+from django.views.static import serve
 
+def cert_view(request):
+    return serve(
+        request,
+        path='files/secure-boot-master-ca.crl',
+        document_root=settings.STATIC_ROOT
+    )
 
 class GreenhouseVacancies(TemplateView):
     template_name = "careers/all-vacancies.html"
